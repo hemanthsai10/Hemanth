51d7789beea5dc47f4eb099acac54d3453258414
Hemanth <hemanthsai10@gmail.com>
2017-03-08 00:07:29 +0530

    Creating admin reports page and students table creation

---
+++ app/controllers/admin/reports_controller.rb
@@ -0,0 +1,11 @@
+class Admin::ReportsController < ApplicationController
+	def update
+	end
+
+	def show
+		render :json => 'fail'
+	end
+
+	def index
+  	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/index'
-  root 'pages#index'
+  	get 'pages/index'
+ 	root 'pages#index'
 
+  namespace :admin do
+  	resources :reports do
+  		get :update, :on => :member
+	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
