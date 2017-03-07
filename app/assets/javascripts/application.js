b61225d69b61e1bf97f3a68e35253808dbcbf508
Hemanth <hemanthsai10@gmail.com>
2017-03-07 22:22:38 +0530

    Header addition to the site

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,21 @@
 //= require tether
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function(){ 
+    var touch 	= $('#touch-menu');
+	var menu 	= $('.menu');
+
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+	
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 767 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+	
+});
