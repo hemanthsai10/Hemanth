498a762fb600f9f8f149b5b5cc80efefe092f58b
Hemanth <hemanthsai10@gmail.com>
2017-03-08 14:36:02 +0530

    Reports update complete page and page layout tempfix

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Admin::ReportsController < ApplicationController
 	def update
 	end
@@ -5,6 +7,18 @@ class Admin::ReportsController < ApplicationController
 	def show
 		render :json => 'fail'
 	end
+
+	def upload
+		file_data = CSV.read(params['file'].tempfile,:headers=>true)
+		hash = Hash[file_data.headers.map.with_index.to_a]    # => {"a"=>0, "b"=>1, "c"=>2}
+		file_data.each { |studnet_info|
+			std = Student.where(:PIN_NO => studnet_info[hash['PIN NO']]).first_or_create
+			file_data.headers.each do |header|
+				std.update_attributes(header.gsub(' ', '_') => studnet_info[hash[header]]) if Student.column_names.include? header
+			end
+		 	std.save
+		 }
+    end
 
 	def index
   	end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :admin do
   	resources :reports do
   		get :update, :on => :member
+  		post :upload, :on => :collection
 	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
