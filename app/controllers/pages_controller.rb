5507b1106b1219694b10b9b7cf28267b390750ab
Hemanth <hemanthsai10@gmail.com>
2017-03-18 20:58:31 +0530

    Adding quotations to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+require 'net/http'
+
   def index
+  	puts "Here it begins"
+  	@quote = eval(Net::HTTP.get(URI.parse('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1')))
+  	@quote = @quote[0]
+  	puts "result is #{@quote}"
   end
 end
